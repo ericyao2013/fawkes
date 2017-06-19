bb71d6e3524fc2ff58bfce2316f42bcc779ac7f5
Matthias Loebach <matthias.loebach@ima-zlw-ifu.rwth-aachen.de>
2017-06-19 16:56:12 +0200

    pddl-robot-memory: increase retries even further

--- src/plugins/pddl-robot-memory/pddl_robot_memory_thread.cpp
+++ src/plugins/pddl-robot-memory/pddl_robot_memory_thread.cpp
@@ -132,8 +132,8 @@ PddlRobotMemoryThread::loop()
       //fill dictionary to expand query template:
       QResCursor cursor = NULL;
       bool query_success = false;
-      // FIXME loops should be configurable
-      for ( size_t loops = 0; !query_success && loops < 15; ++loops) {
+      // FIXME loops should be configurable, 5 minutes max now
+      for ( size_t loops = 0; !query_success && loops < 600; ++loops) {
         cursor = robot_memory->query(fromjson(query_str), collection);
         query_success = cursor->more();
         // Wait 500 millis if query failed
